c543001ccbb63b969be5638975f4d4869d3afc1f
petrovich1986 <artemponurovskij@gmail.com>
2019-03-13 14:30:18 +0800

    24 lesson 1

--- js/gulpfile.js
+++ js/gulpfile.js
@@ -1,8 +1,17 @@
 var gulp = require('gulp');
-
+var cleanCSS = require('gulp-clean-css');
 gulp.task('default', defaultTask);
 
 function defaultTask(done) {
  
   done();
 }
+
+gulp.task('minify-css', function(done){
+   return gulp.src('apartments/src/css/*.css')
+   .pipe(cleanCSS({
+    compatibility: 'ie8'
+   }))
+  .pipe(gulp.dest('dist/css/'))
+  done();
+});
